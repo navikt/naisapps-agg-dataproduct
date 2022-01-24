bb4ad23f92569bdbf80b4670330e9d512deaa0de
Dieffenthaler <louis.maurice.dieffenthaler@nav.no>
2022-01-24 10:41:37 +0100

    WIP on the ETL-tutorial

--- main.py
+++ main.py
@@ -1,4 +1,38 @@
-from workflows import applications_per_cluster
+import pandas_gbq
 
-# ETL 1
-applications_per_cluster.run_etl()
+
+def aggregate_apps(df):
+    """
+    This function aggregates apps per location (on prem or cloud) and enviornment (dev or prod)
+
+    :param df: pd.DataFrame: The dataframe the function is applied on
+    :return: df: pd.Dataframe: Table containing apps per date, cluster and prod/dev
+    """
+    df['antall_apper'] = 1
+
+    # Extracting information about environment
+    df['env'] = 'dev'
+    df.loc[df['cluster'].str.contains('prod'), 'env'] = 'prod'
+
+    # Extracting information about the location
+    df['datacenter'] = 'on_prem'
+    df.loc[df['cluster'].str.contains('gcp'), 'datacenter'] = 'gcp'
+
+    df = df.groupby(['dato', 'env', 'datacenter'])['antall_apper'].count().reset_index()
+
+    return df
+
+
+def run_etl():
+    # Extract
+    query = 'SELECT * FROM `aura-prod-d7e3.dataproduct_apps.dataproduct_apps_unique`'
+    df = pandas_gbq.read_gbq(query)
+
+    # Transform
+    df = aggregate_apps(df)
+
+    # Load
+
+
+if __name__ == '__main__':
+    run_etl()
--- test_logic.py
+++ test_logic.py
@@ -1,32 +1,30 @@
 import unittest
 import pandas as pd
-import aggregations
+
+from main import aggregate_apps
 
 
 class TestAggregations(unittest.TestCase):
     def setUp(self):
-        self.df_raw = pd.read_csv('test_data.csv', separator=',')
-        df = aggregate(df_raw)
-        self.dato = '2022-01-01'
-
+        df_raw = pd.read_csv('test_data.csv', sep=',')
+        df = aggregate_apps(df_raw)
+        dato = '2022-20-01'
+        self.df = df[df['dato'] == dato]
 
     def test_prodcount(self):
         df = self.df
-        antall_apper = df[(df['dato']==self.dato) & (df['env']=='prod')]['antall_apper']
-        self.assertEqual(antall_apper==3)
-
+        antall_apper = df[(df['env'] == 'prod')]['antall_apper'].sum()
+        self.assertTrue(antall_apper == 3)
 
     def test_cloud_count(self):
         df = self.df
-        antall_apper = df[(df['dato']==self.dato) & (df['datacenter']=='gcp')]['antall_apper']
-        self.assertEqual(antall_apper==3)
-
+        antall_apper = df[df['datacenter'] == 'gcp']['antall_apper'].sum()
+        self.assertTrue(antall_apper == 2)
 
     def test_onprem_count(self):
         df = self.df
-        antall_apper = df[(df['dato']==self.dato) & (df['datacenter']=='onprem')]['antall_apper']
-        self.assertEqual(antall_apper==3)
-
+        antall_apper = df[df['datacenter'] == 'on_prem']['antall_apper'].sum()
+        self.assertTrue(antall_apper == 4)
 
 
 if __name__ == '__main__':
