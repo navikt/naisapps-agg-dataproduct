2488ff4ea2bd03fae3bc9893b88d36fb6eb24644
Gøran Berntsen <gorzan@gmail.com>
2022-01-27 13:45:17 +0100

    load to bq
    Co-authored-by: Louis Maurice Dieffenthaler <louis.maurice.dieffenthaler@nav.no>

--- main.py
+++ main.py
@@ -23,16 +23,37 @@ def count_apps(df):
     return df
 
 
+def load_data(df):
+    project_id = 'nais-analyse-prod-2dcc'
+    dataset = ''
+    table = ''
+
+    destination_table = f'{project_id}.{dataset}.{table}'
+
+    table_schema = [
+        {'name': 'dato', 'type': 'DATE'},
+        {'name': 'env', 'type': 'STRING'},
+        {'name': 'datacenter', 'type': 'STRING'},
+        {'name': 'antall_apper', 'type': 'INT'}
+    ]
+
+    df.to_gbq(
+        destination_table=destination_table,
+        table_schema=table_schema,
+        if_exists='replace'
+    )
+
+
 def run_etl():
     # Extract
     query = 'SELECT * FROM `aura-prod-d7e3.dataproduct_apps.dataproduct_apps_unique`'
     df = pandas_gbq.read_gbq(query, project_id='nais-analyse-prod-2dcc')
 
     # Transform
-    #df = aggregate_apps(df)
-    print(df.head())
-    # Load
+    df = count_apps(df)
 
+    # Load
+    load_data(df)
 
 if __name__ == '__main__':
     run_etl()
